8f2391774ab1ada3896a6d5114f788875041216c
limon-programming-hero <limonhasan405@gmail.com>
2023-06-11 13:09:16 +0600

    [done] mongodb interface and model created

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,8 @@
+// import { Request, Response } from "express";
+// import { CreateUserFun } from "./user.service";
+
+// const createUser = async(req:Request,res:Response) =>{
+//   const user = await req.body;
+//   const result = await CreateUserFun(user);
+//   if(result)
+// }
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import mongoose from 'mongoose'
 import dotenv from './config/index'
 import app from './app'
 
-async function boostrap(): Promise<void> {
+async function bootstrap(): Promise<void> {
   try {
     await mongoose.connect(dotenv.database_url as string)
+    // app.use('/app/v1/user')
     app.listen(dotenv.port, () => {
       console.log(`Application listening on port ${dotenv.port}`)
     })
@@ -13,4 +14,4 @@ async function boostrap(): Promise<void> {
   }
 }
 
-boostrap()
+bootstrap()
